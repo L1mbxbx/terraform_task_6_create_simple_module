571cabb46794875920af60790c7f84bffbb09867
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-05 12:43:01 +0300

    add module

---
+++ modules/resource_group_storage/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+    description = "The name of the resource group"
+    type        = string
+    default     = "example-resource-group"
+}
+
+variable "location" {
+    description = "The Azure region where the resource group will be created"
+    type        = string
+    default     = "East US"
+}
+
+variable "storage_account_name" {
+    description = "The name of the storage account to be created in the resource group"
+    type        = string
+    default     = "examplestorageaccount"
+}
