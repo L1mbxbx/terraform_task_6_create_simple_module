3c70c9c840aa228e38f4fda49db2be0f5fe3141e
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-05 13:25:18 +0300

    Implement module

--- main.tf
+++ main.tf
@@ -10,10 +10,15 @@ terraform {
 provider "azurerm" {
   features {
     resource_group {
-        prevent_deletion_if_contains_resources = true
+      prevent_deletion_if_contains_resources = true
     }
   }
 }
 
 module "resource_group_storage" {
+  source               = "L1mbxbx/resource_group_storage/azurerm"
+  version              = "1.0.0"
+  location             = "West Europe"
+  resource_group_name  = "limbx-terraform-rg"
+  storage_account_name = "limbxterraformstorage100"
 }
